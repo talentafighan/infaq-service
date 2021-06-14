82bc0c102f12f6f8c4c77bfa2eac6d38dd6db072
fighantalenta_ <tatamf.id@gmail.com>
2021-06-14 12:32:32 +0700

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mmongodb+srv://admin:admin@infaq-cluster-v1.g5tjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
---
+++ models/users/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const UserSchema = new schema({
+  user_id: {
+    type: schema.ObjectId,
+  },
+  user_type: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  mobile_phone_number: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
