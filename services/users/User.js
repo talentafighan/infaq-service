7086cd365a9b8a6259bb165f9850b3a9250aaa2d
Prasetya234 <putraprasetyadhany@gmail.com>
2021-06-19 19:25:23 +0700

    register and send email

--- services/users/User.js
+++ services/users/User.js
@@ -19,21 +19,21 @@ module.exports = {
           to: newUser.email,
           subject: "Infaq Service ✔",
           text: "your account has been registered in Infaq",
-          html: "<p>For clients that do not support AMP4EMAIL or amp content is not valid</p>",
-          amp: `<!doctype html>
-          <html ⚡4email>
-            <head>
-              <meta charset="utf-8">
-              <style amp4email-boilerplate>body{visibility:hidden}</style>
-              <script async src="https://cdn.ampproject.org/v0.js"></script>
-              <script async custom-element="amp-anim" src="https://cdn.ampproject.org/v0/amp-anim-0.1.js"></script>
-            </head>
-            <body>
-              <p>Image: <amp-img src="https://cldup.com/P0b1bUmEet.png" width="16" height="16"/></p>
-              <p>GIF (requires "amp-anim" script in header):<br/>
-                <amp-anim src="https://cldup.com/D72zpdwI-i.gif" width="500" height="350"/></p>
-            </body>
-          </html>`,
+          // html: "<p>For clients that do not support AMP4EMAIL or amp content is not valid</p>",
+          // amp: `<!doctype html>
+          // <html ⚡4email>
+          //   <head>
+          //     <meta charset="utf-8">
+          //     <style amp4email-boilerplate>body{visibility:hidden}</style>
+          //     <script async src="https://cdn.ampproject.org/v0.js"></script>
+          //     <script async custom-element="amp-anim" src="https://cdn.ampproject.org/v0/amp-anim-0.1.js"></script>
+          //   </head>
+          //   <body>
+          //     <p>Image: <amp-img src="https://cldup.com/P0b1bUmEet.png" width="16" height="16"/></p>
+          //     <p>GIF (requires "amp-anim" script in header):<br/>
+          //       <amp-anim src="https://cldup.com/D72zpdwI-i.gif" width="500" height="350"/></p>
+          //   </body>
+          // </html>`,
         };
         await transporter.sendMail(mailOptions, function (err, info) {
           if (err) {
